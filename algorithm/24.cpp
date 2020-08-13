18ae9354e2b87b6df8619dc02a23b388b85e4c37
mirana <465049914@qq.com>
2020-08-14 00:18:33 +0800

    8/13

--- algorithm/24.cpp
+++ algorithm/24.cpp
@@ -1,27 +1,31 @@
 /**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
- */
+* Definition for singly-linked list.
+* struct ListNode {
+*     int val;
+*     ListNode *next;
+*     ListNode(int x) : val(x), next(NULL) {}
+* };
+*/
 class Solution {
 public:
-    ListNode* swapPairs(ListNode* head) {
-        if(head==NULL||head->next==NULL) return head;
-        ListNode* p=head;
-        ListNode* q=head->next;
-        head=q;
-        while(p!=NULL&&q!=NULL) {
-            p->next=q->next;
-            q->next=p;
-            if(p->next!=NULL) {
-                p=p->next;
-                q=p->next;
-            }
-            else return head;
-        }
-        return head;
-    }
+	ListNode* swapPairs(ListNode* head) {
+		if (head == NULL || head->next == NULL) return head;
+		ListNode* p = head;
+		ListNode* q = head->next;
+		ListNode* dummyhead = new ListNode(0);
+		dummyhead->next = head;
+		ListNode* pos = dummyhead;
+		while (p != NULL&&q != NULL) {
+			pos->next = q;
+			p->next = q->next;
+			q->next = p;
+			if (p->next != NULL) {
+				pos = p;
+				p = p->next;
+				q = p->next;
+			}
+			else return dummyhead->next;
+		}
+		return dummyhead->next;
+	}
 };
