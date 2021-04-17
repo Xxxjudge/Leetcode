10df04e64a2f4f89a3a382f46b626ab58b68c3f9
cauchyschwarz <465049914@qq.com>
2021-04-17 11:30:56 +0800

    change

--- algorithm/121.cpp
+++ algorithm/121.cpp
@@ -1,28 +1,12 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int n=prices.size();
-        if(n==0) return 0;
-        stack<int> s;
-        s.push(prices[0]);
-        int bottom=s.top();
-        int max=0;
-        vector<int> temp=prices;
-        temp.push_back(0);
-        for(int i=1;i<=n;i++) {
-            if(temp[i]>=s.top()) s.push(temp[i]);
-            else {
-                while(!s.empty()&&temp[i]<s.top()) {
-                    max=(max>(s.top()-bottom))?max:(s.top()-bottom);
-                    s.pop();
-                }
-                if(s.empty()) {
-                    s.push(temp[i]);
-                    bottom=s.top();
-                } else s.push(temp[i]);
-            }
+        int minprice = 1e9;
+        int maxprofit = 0;
+        for (auto x:prices) {
+            minprice=min(minprice,x);
+            maxprofit=max(maxprofit,x-minprice);
         }
-        return max;
-
+        return maxprofit;
     }
 };
--- algorithm/122.cpp
+++ algorithm/122.cpp
@@ -11,3 +11,4 @@ public:
         return dp0;
     }
 };        
+//dp
