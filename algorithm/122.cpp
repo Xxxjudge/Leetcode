a708c493589033b608122662dd7e00888c32f659
cauchyschwarz <465049914@qq.com>
2021-04-17 11:29:40 +0800

    Update 122.cpp

--- algorithm/122.cpp
+++ algorithm/122.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        vector<vector<int>> dp(prices.size(),vector<int>(2));
-        dp[0][0]=0,dp[0][1]=-prices[0];
+        int dp0=0,dp1=-prices[0];
         for(int i=1;i<prices.size();i++) {
-            dp[i][0]=max(dp[i-1][0],dp[i-1][1]+prices[i]);
-            dp[i][1]=max(dp[i-1][0]-prices[i],dp[i-1][1]);
+            int newdp0=max(dp0,dp1+prices[i]);
+            int newdp1=max(dp0-prices[i],dp1);
+            dp0=newdp0;
+            dp1=newdp1;
         }
-        return dp[prices.size()-1][0];
+        return dp0;
     }
-};
+};        
