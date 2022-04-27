76060925ca9ea44b569cfbfcd8fc9fba30f55600
Xxxjudge <465049914@qq.com>
2022-04-27 23:45:30 +0800

    略
    略

---
+++ algorithm/1880.检查某单词是否等于两单词之和.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=1880 lang=cpp
+ *
+ * [1880] 检查某单词是否等于两单词之和
+ */
+
+// @lc code=start
+class Solution {
+public:
+    bool isSumEqual(string firstWord, string secondWord, string targetWord) {
+
+    }
+};
+// @lc code=end
+
