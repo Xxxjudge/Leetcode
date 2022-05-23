468f2453525e7091af9c6ad80398ad824d869684
Xxxjudge <465049914@qq.com>
2022-05-23 18:45:07 +0800

    组合总和
    dp背包问题

---
+++ algorithm/377.组合总和-ⅳ.cpp
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode.cn id=377 lang=cpp
+ *
+ * [377] 组合总和 Ⅳ
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int combinationSum4(vector<int>& nums, int target) {
+        vector<int> dp(target+1,0);
+        dp[0]=1;
+        for(int i=1;i<target+1;i++) {
+            for(int j=0;j<nums.size();j++) {
+                if(i>=nums[j]&&dp[i]<INT_MAX-dp[i-nums[j]]) dp[i]+=dp[i-nums[j]];
+            }
+        }
+        return dp[target];
+    }
+};
+// @lc code=end
+
